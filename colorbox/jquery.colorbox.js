0b12c26d9e6a8db48c7df85be59da7cc4f91b4a5
kenrii <ken.j.riippa@gmail.com>
2020-05-12 11:52:50 +0200

    colrobox

--- colorbox/jquery.colorbox.js
+++ colorbox/jquery.colorbox.js
@@ -1102,4 +1102,36 @@
 
 	publicMethod.settings = defaults;
 
+	(function ($, window, document, undefined) {
+		//Configure colorbox call back to resize with custom dimensions 
+		  $.colorbox.settings.onLoad = function() {
+			colorboxResize();
+		  }
+		
+		  //Customize colorbox dimensions
+		  var colorboxResize = function(resize) {
+			var width = "90%";
+			var height = "90%";
+		
+		if($(window).width() > 960) { width = "75%" }
+		if($(window).height() > 700) { height = "55%" } 
+		
+		$.colorbox.settings.height = height;
+		$.colorbox.settings.width = width;
+		
+		//if window is resized while lightbox open
+		if(resize) {
+		  $.colorbox.resize({
+			'height': height,
+			'width': width
+			  });
+			} 
+		  }
+		
+			//In case of window being resized
+		  $(window).resize(function() {
+			colorboxResize(true);
+		  });
+		
+		})(jQuery, this, this.document);
 }(jQuery, document, window));
--- index.js
+++ index.js
@@ -144,3 +144,14 @@ let liity = document.querySelector("#ToLiity")
 liity.addEventListener("click", () => {
     smoothScroll("#Liity", 1000)
 })
+
+// Colorbox UKK esiin saamiseen
+$(document).ready(function(){
+    //Examples of how to assign the Colorbox event to elements
+    $(".inline").colorbox({inline:true, width:"80%", height:"80%"});
+    //Example of preserving a JavaScript event for inline calls.
+    $("#click").click(function(){ 
+    $('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
+    return false;
+    });
+});
